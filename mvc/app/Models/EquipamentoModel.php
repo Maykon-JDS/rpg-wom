4d09d9e3314f348764448fa2e540b1444c5c1f91
Maykon Dias <62573242+Maykon-JDS@users.noreply.github.com>
2023-06-05 23:26:28 -0300

    Inclusion: creation of the files 'EquipamentoModel' and of the migration '...create_equipamentos_table.php'

---
+++ mvc/app/Models/EquipamentoModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EquipamentoModel extends Model
+{
+    use HasFactory;
+}
